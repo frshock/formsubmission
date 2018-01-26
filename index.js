482cdc664c3d94af4bb5853f6491bdbd7b5608cf
frshock <will.s.crane@gmail.com>
2018-01-26 10:46:51 -0500

    pelase

--- index.js
+++ index.js
@@ -4,21 +4,34 @@ const express = require('express'),
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send('Come on man, this is the wrong route!')
 });
 
 app.get('/processform', (req,res)=>{
-    console.log(req.query)
-    const par = req.query
+    const {user, bio, agree, city} = req.query
     const sendBack = {
-        user: par.user ? par.user : 'default user',
-        bio: par.bio ? par.bio : 'Lorem Ipsum goes here.',
-        agree: par.agree === 'on' ? 'agreed' : 'disagreed',
-        city: par.city ? par.city : 'Unknown'
+        user: par.user ? user : 'default user',
+        bio: par.bio ? bio : 'Lorem Ipsum goes here.',
+        agree: agree === 'on' ? 'agreed' : 'disagreed',
+        city: city ? city : 'Unknown'
     }
-    res.render('pages/index', {data: sendBack});
+    res.render('pages/index', {data: sendBack, method: 'GET', mechanism: 'query'});
+})
+
+app.post('/processform', (req,res)=>{
+    const {user, bio, agree, city} = req.body
+    const sendBack = {
+        user: user ? user : 'default user',
+        bio: bio ? bio : 'Lorem Ipsum goes here.',
+        agree: agree === 'on' ? 'agreed' : 'disagreed',
+        city: city ? city : 'Unknown'
+    }
+    res.render('pages/index', {data: sendBack, method: 'POST', mechanism: 'body'});
 })
 
 app.listen(PORT, ()=> console.log('Listening on some port.'))
